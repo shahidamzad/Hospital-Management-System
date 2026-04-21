f8e1c6741072ce2f249918b7f578b1db956c1b69
Your Name <shahidamzad07@gmail.com>
2026-04-21 11:16:14 +0530

    complete listof Doctors

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../Components/Header.jsx'
 import Speciality from '../Components/Speciality.jsx'
+import TopDoctors from '../Components/TopDoctors.jsx'
+
 
 const Home = () => {
   return (
     <div>
        <Header />
        <Speciality />
+       <TopDoctors />
         
     </div>
   )
