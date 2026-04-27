ca82cc53d99067883ee19cfe098c9c67b9778f08
Your Name <shahidamzad07@gmail.com>
2026-04-27 22:19:29 +0530

    profile page

--- client/src/Pages/MyProfile.jsx
+++ client/src/Pages/MyProfile.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+  const [userData, setUserData] = useState({
+    name: "Shahid Hussain",
+    image: assets.profile_pic,
+    emial: 'shahidamzad07@gmail.com',
+    phone: '+91 91000-87656',
+    address: {
+      line1: "57th cross , Richad",
+      line2: "Circle , Crurch Road , london"
+    },
+    gender: 'Male',
+    dob: '2003-10-30'
+  })
+
+  const [isEdit, setIsEdit] = useState(false)
+
   return (
-    <div>
-         profile page 
+    <div className='max-w-lg flex flex-col gap-2 text-sm'>
+      <img className='w-36 rounded' src={userData.image} alt="" />
+
+      {
+        isEdit ? <input className='bg-gray-50  text-3xl font-medium max-w-60 mt-4' value={userData.name} onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))} type="text" /> : <p className='font-medium text-3xl text-neutral-800' >{userData.name}</p>
+      }
+      <hr className='bg-zinc-400 h-[1px] border-none ' />
+      <div >
+        <p className='text-neutral-500 underline mt-3'>CONTACT INFORMATION </p>
+        <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700 '>
+          <p className='font-medium'>Email id:</p>
+          <p className='text-blue-600'> {userData.emial}</p>
+          <p className='font-medium'>Phone no:</p>
+          {
+            isEdit ? <input className='bg-gray-100 max-w-52' type="text" value={userData.phone} onChange={e => setUserData(prev => ({ ...prev }))} /> : <p className='text-blue-500'> {userData.phone} </p>
+          }
+          <p className='font-medium'>Address</p>
+          {
+            isEdit
+              ? <p>
+                <input className='bg-gray-50' type="text" onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line1: e.target.value } }))} value={userData.address.line1} />
+                <br />
+                <input className='bg-gray-50' type="text" onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line2: e.target.value } }))} value={userData.address.line2} />
+              </p>
+              : <p className='text-gray-500'>
+                {userData.address.line1}
+                <br />
+                {userData.address.line2}
+              </p>
+          }
+        </div>
+      </div>
+
+      <div>
+        <p className='text-neutral-500 underline mt-3'>BASIC INFORMATION</p>
+        <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
+          <p>Gender : </p>
+          {
+            isEdit
+              ? <select className='max-w-20 bg-gray-100' onChange={(e) => setUserData(prev => ({ ...prev, gender: e.target.value }))} value={userData.gender} >
+                <option value="Male">Male</option>
+                <option value="Female">female</option>
+                <option value="Other">Other</option>
+              </select>
+              : <p className='text-gray-400'>{userData.gender}</p>
+          }
+          <p className='font-medium'>BirthDay</p>
+          {
+            isEdit
+              ? <input className='max-w-28 bg-gray-100' type="date" onChange={(e) => setUserData(prev => ({ ...prev, dob: e.target.value }))} value={userData.dob} />
+              : <p className='text-gray-400'> {userData.dob} </p>
+          }
+        </div>
+      </div>
+      <div className='mt-10'>
+        {
+          isEdit ? <button className='border border-primary px-8 py-2 rounded-full  hover:bg-primary  hover:text-white transition-all duration-500' onClick={() => setIsEdit(false)} >Save Information</button>
+            : <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary   hover:text-white transition-all duration-500' onClick={() => setIsEdit(true)} >Edit</button>
+        }
+      </div>
     </div>
   )
 }
