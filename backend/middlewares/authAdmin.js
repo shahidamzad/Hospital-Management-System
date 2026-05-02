3db3c8cb975d246aef65d9c23dad4f9b7d5ca2ba
Your Name <shahidamzad07@gmail.com>
2026-05-02 21:29:59 +0530

    adminfolder , backend code

---
+++ backend/middlewares/authAdmin.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+
+
+
+// admin authantication middleware 
+
+export const authAdmin = async (req , res , next)=>{
+    try {
+        
+        const {atoken} = req.headers;
+        
+        if (!atoken) {
+            return res.json({success:false,message:"not authrized login again"})  
+        }
+
+        const token_decode = jwt.verify(atoken,process.env.JWT_SECRET);
+
+        if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+            return res.json({success:false , message:"not authrized login again"})
+        }
+
+        next()
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false , message:error.message})
+        
+    }
+}
--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import upload from '../middlewares/multer.js'
 import { addDoctor, adminLogin } from '../controllers/admin.Controller.js';
+import { authAdmin } from '../middlewares/authAdmin.js';
 
 
 const adminRouter = express.Router();
 
-adminRouter.post('/add-doctor',upload.single('image'),addDoctor);
-adminRouter.post('/login',adminLogin);
+adminRouter.post('/add-doctor',authAdmin,upload.single('image'),addDoctor);
+adminRouter.post('/login' ,adminLogin);
 
 
 
